1219d087bda4458dfade2d656fc46d117bdcdf5c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-15 16:27:05 +1000

    Change title of the page

--- src/components/HeaderSection/index.js
+++ src/components/HeaderSection/index.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Head from "next/head";
 
 const HeaderSection = () => {
   return (
-    <div id='header-wrapper' className='bg-purple-500'>
-      <header className='bg-purple-500 lg:container mx-auto text-white py-5'>
-        <h2 className='text-xl'>Abhishekh Maharjan</h2>
-      </header>
-    </div>
+    <React.Fragment>
+      <Head>
+        <title>Abhishekh Maharjan</title>
+      </Head>
+      <div id='header-wrapper' className='bg-purple-500'>
+        <header className='bg-purple-500 lg:container mx-auto text-white py-5'>
+          <h2 className='text-xl'>Abhishekh Maharjan</h2>
+        </header>
+      </div>
+    </React.Fragment>
   );
 };
 
