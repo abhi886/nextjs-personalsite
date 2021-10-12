2bafce0377841d9b2c1d7769c5e19b0080538ed0
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-10-12 11:29:56 +1100

    Add Header Animations and typed text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../public/images/logo.png";
 import background from "../../public/images/background.jpeg";
+import Typed from "react-typed";
 
 import { ChevronDownIcon } from "@heroicons/react/outline";
 
@@ -44,16 +45,39 @@ function HeroSection() {
       </div>
       <div className='absolute top-1/3 w-full text-center'>
         {" "}
-        <h1 className='text-white font-extrabold text-4xl stroke-2 md:font-extrabold md:text-6xl'>
+        <h1 className='text-white font-extrabold text-4xl stroke-2 md:font-extrabold md:text-6xl animate-fade-in-up'>
           I'm Abhishekh Maharjan.
         </h1>
-        <h2 className='font-medium p-6 text-gray-300'>
-          <span className='text-gray-400'>Based on Canberra. </span> Front-end
-          Developer with an ambition of becoming a FULL Stack Developer.{" "}
-        </h2>
+        <div className='grid grid-cols-2 p-3'>
+          <div className='text-right'>
+            <h2 className='font-medium text-gray-300'>Based on Canberra.</h2>
+          </div>
+          <div className='text-left'>
+            <Typed
+              className='font-medium pl-2 text-gray-300'
+              strings={[
+                "Front-end Developer",
+                "Full Stack Developer",
+                "Software Engineer",
+                "And more",
+              ]}
+              typeSpeed={100}
+              backSpeed={50}
+              loop
+            />
+          </div>
+        </div>
         {/* <p className='font-medium p-2 text-gray-100'>Software Engineer</p> */}
+        <div className='space-x-2'>
+          <button className='text-black bg-white px-3 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
+            Contact Me
+          </button>
+          <button className='text-black bg-white px-4 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
+            Resume
+          </button>
+        </div>
         {/* Social Icons */}
-        <div className=' text-center space-x-6'>
+        <div className=' text-center space-x-6 pt-4'>
           <FontAwesomeIcon
             icon={("fab", faFacebookSquare)}
             size='2x'
@@ -72,14 +96,6 @@ function HeroSection() {
             // color='#4078c0'
             color='#ffffff'
           />
-        </div>
-        <div className='space-x-2'>
-          <button className='text-black bg-white px-3 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
-            Contact Me
-          </button>
-          <button className='text-black bg-white px-4 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
-            Resume
-          </button>
         </div>
       </div>
       {/* scrollDown Button */}
