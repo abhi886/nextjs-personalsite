b75a40d81ca2f30644476ba6900fa09d3b36ecce
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-02-10 10:57:55 +1100

    Implement Incremental Site Regeneration

---
+++ src/components/Skeleton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Skeleton() {
+  return (
+    <div class='w-60 h-24 border-2 rounded-md mx-auto mt-20'>
+      <div class='flex animate-pulse flex-row items-center h-full justify-center space-x-5'>
+        <div class='w-12 bg-gray-300 h-12 rounded-full '></div>
+        <div class='flex flex-col space-y-3'>
+          <div class='w-36 bg-gray-300 h-6 rounded-md '></div>
+          <div class='w-24 bg-gray-300 h-6 rounded-md '></div>
+        </div>
+      </div>
+    </div>
+  );
+}
