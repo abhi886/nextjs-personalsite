c4af39a688b0f3207c102519ee94fe357cb69bad
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-08-01 11:51:11 +1000

    Add Header and Footer Section

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import HeaderSection from "../../src/components/HeaderSection";
+import FooterSection from "../../src/components/FooterSection";
 
 function index() {
-  return <div>index</div>;
+  return (
+    <>
+      <div className='px-4 py-2 bg-personal_blue'>
+        <HeaderSection></HeaderSection>
+      </div>
+      <div>
+          
+      </div>
+      <div className='bg-personal_blue'>
+        <FooterSection />
+      </div>
+    </>
+  );
 }
 
 export default index;
