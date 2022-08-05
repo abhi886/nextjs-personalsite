8437527294c4813bf2dd88e14b29de1d2cb76475
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-08-05 11:47:16 +1000

    Blog Section Complete

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,15 +1,15 @@
 import React from "react";
 import Head from "next/dist/shared/lib/head";
+import { createClient } from "contentful";
 import HeaderSection from "../../src/components/HeaderSection";
 import FooterSection from "../../src/components/FooterSection";
 import myPhoto from "../../public/images/me.jpg";
 import { RewindIcon, ClockIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
-import { createClient } from "contentful";
 import convertDate from "../../src/utils/convertDate";
+import { renderOptions } from "../../src/utils/contentfulUtils";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -53,57 +53,6 @@ export async function getStaticProps({ params }) {
     revalidate: 1,
   };
 }
-const renderOptions = {
-  renderNode: {
-    [INLINES.EMBEDDED_ENTRY]: (node, children) => {
-      // target the contentType of the EMBEDDED_ENTRY to display as you need
-      if (node.data.target.sys.contentType.sys.id === "blogPost") {
-        return (
-          <a href={`/blog/${node.data.target.fields.slug}`}>
-            {" "}
-            {node.data.target.fields.title}
-          </a>
-        );
-      }
-    },
-    [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-      // target the contentType of the EMBEDDED_ENTRY to display as you need
-      if (node.data.target.sys.contentType.sys.id === "codeBlock") {
-        return (
-          <pre>
-            <code>{node.data.target.fields.code}</code>
-          </pre>
-        );
-      }
-
-      if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
-        return (
-          <iframe
-            src={node.data.target.fields.embedUrl}
-            height='100%'
-            width='100%'
-            frameBorder='0'
-            scrolling='no'
-            title={node.data.target.fields.title}
-            allowFullScreen={true}
-          />
-        );
-      }
-    },
-
-    [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-      // render the EMBEDDED_ASSET as you need
-      return (
-        <img
-          src={`https://${node.data.target.fields.file.url}`}
-          height={node.data.target.fields.file.details.image.height}
-          width={node.data.target.fields.file.details.image.width}
-          alt={node.data.target.fields.description}
-        />
-      );
-    },
-  },
-};
 
 export default function blogSlug({ blogs }) {
   const { title, readTime, blogImage, blogDescription } = blogs.fields;
