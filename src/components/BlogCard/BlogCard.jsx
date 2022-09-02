0fcdae296f691f9351dffc8ca5e141695ed14f8c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-09-02 17:34:21 +1000

    Add hover to blogs index page

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -13,8 +13,8 @@ function BlogCard({
   updatedAt,
 }) {
   return (
-    <article className='cursor-pointer group'>
-      <div className=' relative overflow-hidden transition-all bg-white-100 rounded-md  hover:scale-105 aspect-video '>
+    <article className='cursor-pointer group  hover:scale-105 transition duration-700 ease-in-out hover:shadow-md '>
+      <div className=' relative overflow-hidden transition-all bg-white-100 rounded-md  aspect-video '>
         <a href={`/blogs/${slug}`}>
           <Image
             src={`https:${blogImage.fields.file.url}`}
@@ -22,35 +22,37 @@ function BlogCard({
             objectFit='contain'
             // width={blogImage.fields.file.details.image.width}
             // height={blogImage.fields.file.details.image.height}
-            layout="fill"
+            layout='fill'
           ></Image>
         </a>
       </div>
-      <div>
+      <div className='group-hover:px-2 hover:transition duration-700 ease-in-out group-hover:mb-6'>
         <a href='#'>
           <span className=' text-blue-600 inline-block mt-5 text-xs font-medium tracking-wider uppercase'>
             Javascript
           </span>
         </a>
-      </div>
-      <h2 className='mt-2 text-lg font-semibold tracking-normal text-brand-primary '>
-        <span className=' bg-gradient-to-r from-green-200 to-green-100   bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]'>
-          {title}
-        </span>
-      </h2>
+        <div className='group overflow-hidden text-overflow:ellipsis h-16 line-clamp-2'>
+          <h2 className='mt-2 text-lg font-semibold tracking-normal text-brand-primary'>
+            <span className=' bg-gradient-to-r from-green-200 to-green-100   bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]'>
+              {title}
+            </span>
+          </h2>
+        </div>
 
-      <div className='flex items-center mt-4 space-x-4 text-gray-500 '>
-        <Image
-          src={myPhoto}
-          alt='Abhishekh Maharjan Photo'
-          width={20}
-          height={20}
-          objectFit='contain'
-          className='rounded-full'
-        />
-        <span className='text-sm'>Abhishekh Maharjan</span>
-        <span className='text-xs text-gray-300'>•</span>
-        <time className='text-sm'>{convertDate(updatedAt)}</time>
+        <div className='flex items-center mt-4 space-x-4 text-gray-500'>
+          <Image
+            src={myPhoto}
+            alt='Abhishekh Maharjan Photo'
+            width={20}
+            height={20}
+            objectFit='contain'
+            className='rounded-full'
+          />
+          <span className='text-sm'>Abhishekh Maharjan</span>
+          <span className='text-xs text-gray-300'>•</span>
+          <time className='text-sm'>{convertDate(updatedAt)}</time>
+        </div>
       </div>
     </article>
   );
