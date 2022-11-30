ae8aa376befc66495942c1c6f199607ceee1186e
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-11-30 18:05:15 +1100

    Blog Post: Finalize design for search blogs field

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -6,6 +6,7 @@ import BlogCard from "../../src/components/BlogCard/BlogCard";
 import Layout from "../../src/components/OtherPageLayout";
 import { NextSeo } from "next-seo";
 import logo from "../../public/images/logo.png";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -60,16 +61,27 @@ function index({ blogs, seoData }) {
         }}
       />
       <section className='px-4 lg:px-44 py-5 lg:py-8 '>
-        <div class='py-6 md:py-12 '>
-          <h2 class='text-2xl md:text-4xl font-bold font-sans text-personal_blue md:leading-tight'>
+        <div className='py-1'>
+          <p className='text-xl  md:text-4xl font-bold font-sans text-personal_blue md:leading-tight'>
             <strong>Blog</strong> Posts
-          </h2>
-
-          <h2 class='text-xl text-gray-600 mt-2'>
-            I write about modern{" "}
-            <strong>FULL Stack Web and Software Development.</strong> Subscribe
-            to my newsletter to make sure you dont miss anything
-          </h2>
+          </p>
+          <p class='text-base md:text-xl text-gray-600 mt-2'>
+            I write articles about modern Full Stack Web Development. View my
+            recent articles and blog posts.
+            {/* Subscribe to my newsletter to make sure you dont miss anything */}
+          </p>
+        </div>
+        <div className='flex justify-center mt-2'>
+          <div className='mb-3 w-full '>
+            <input
+              type='search'
+              className='block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
+        border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      '
+              id='searchPosts'
+              placeholder='Search'
+            />
+          </div>
         </div>
         <div className='grid gap-10 lg:gap-10 md:grid-cols-2 lg:grid-cols-3 '>
           {blogs.map((blog, i) => (
