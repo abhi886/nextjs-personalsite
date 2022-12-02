a5d81c8037ded9f307f5a95b57e6980a39bfff3c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-12-02 11:29:58 +1100

    Complete search functionality

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import HeaderSection from "../../src/components/HeaderSection";
 import FooterSection from "../../src/components/FooterSection";
 import { createClient } from "contentful";
@@ -7,6 +7,7 @@ import Layout from "../../src/components/OtherPageLayout";
 import { NextSeo } from "next-seo";
 import logo from "../../public/images/logo.png";
 import { SearchIcon } from "@heroicons/react/outline";
+import SearchBox from "../../src/components/SearchBox/SearchBox";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -27,6 +28,8 @@ export async function getStaticProps() {
 }
 
 function index({ blogs, seoData }) {
+  const [searchQuery, SetSearchQuery] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState(blogs);
   const blogPageSEOData = seoData.filter(
     (data) => data.fields.whichPage == "Blogs Page"
   );
@@ -38,7 +41,25 @@ function index({ blogs, seoData }) {
     pageSeoImageType,
     pageImageAltText,
   } = blogPageSEOData[0].fields;
+  const handleSearch = (query) => {
+    SetSearchQuery(query);
+  };
 
+  const getFilteredBlogs = useMemo(() => {
+    return filteredBlogs.filter((b) =>
+      b.fields.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+    );
+  }, [filteredBlogs, searchQuery]);
+  const { totalCount, data } = useMemo(() => {
+    if (!filteredBlogs) return { totalCount: 0, data: {} };
+    let filtered = filteredBlogs;
+    if (searchQuery) {
+      filtered = getFilteredBlogs;
+    }
+    // const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+    // const finalData = paginate(sorted, currentPage, PAGESIZE);
+    return { totalCount: filtered.length, data: filtered };
+  }, [filteredBlogs, searchQuery]);
   return (
     <>
       <NextSeo
@@ -60,32 +81,24 @@ function index({ blogs, seoData }) {
           type: "article",
         }}
       />
-      <section className='px-4 lg:px-44 py-5 lg:py-8 '>
-        <div className='py-1'>
-          <p className='text-xl  md:text-4xl font-bold font-sans text-personal_blue md:leading-tight'>
+      <section className='px-4 py-5 min-h-screen lg:px-44 lg:py-8'>
+        <div>
+          <p className='text-xl text-personal_blue md:text-4xl md:leading-tight'>
             <strong>Blog</strong> Posts
           </p>
-          <p class='text-base md:text-xl text-gray-600 mt-2'>
+          <p class='text-base text-gray-600 my-2 md:text-xl'>
             I write articles about modern Full Stack Web Development. View my
             recent articles and blog posts.
-            {/* Subscribe to my newsletter to make sure you dont miss anything */}
           </p>
         </div>
-        <div className='flex justify-center mt-2'>
-          <div className='mb-3 w-full '>
-            <input
-              type='search'
-              className='block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding
-        border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      '
-              id='searchPosts'
-              placeholder='Search'
-            />
-          </div>
-        </div>
+        <SearchBox
+          totalCount={totalCount}
+          value={searchQuery}
+          onChange={handleSearch}
+        ></SearchBox>
         <div className='grid gap-10 lg:gap-10 md:grid-cols-2 lg:grid-cols-3 '>
-          {blogs.map((blog, i) => (
-            <BlogCard key={i} {...blog.fields} {...blog.sys}></BlogCard>
+          {data.map((fb, i) => (
+            <BlogCard key={i} {...fb.fields} {...fb.sys}></BlogCard>
           ))}
         </div>
       </section>
