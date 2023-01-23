648f635a3a54d6d4dcf8a56f3d1457347aa14bca
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-01-23 12:31:19 +1100

    Configure Prettier

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Layout from "../../src/components/LandingPageLayout";
-import ContactMe from "../../src/components/ContactMe";
+import React from 'react';
+import Layout from '../../src/components/LandingPageLayout';
+import ContactMe from '../../src/components/ContactMe';
 
 function index() {
   return <ContactMe></ContactMe>;
