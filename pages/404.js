1ea83ac8e698ecb9420d701dd7d4caf869a1a5d7
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-04-23 12:28:53 +1000

    Improvise 404 not found page

--- pages/404.js
+++ pages/404.js
@@ -1,37 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/dist/shared/lib/head';
 import { useRouter } from 'next/router';
-import HeaderSection from '../src/components/HeaderSection/index';
-import FooterSection from '../src/components/FooterSection/index';
+import Layout from '../src/components/LandingPageLayout/index';
 
 export default function NotFound() {
+  const [time, setTime] = useState(5);
   const router = useRouter();
   useEffect(() => {
-    setTimeout(() => {
+    if (time === 1) {
       router.push('/');
-    }, 4000);
-  }, []);
+    }
+    const timer = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [time]);
   return (
-    <>
-      <Head>
-        <title>404 Not Found</title>
-      </Head>
-      <HeaderSection />
+    <Layout>
       <section className="mx-auto px-8 md:px-8 xl:px-12 xl:max-w-6xl h-screen">
-        <div className="dark:bg-personal_blue border shadow-gray-600 dark:shadow-gray-400 origin-bottom -rotate-2 text-center relative top-1/3 h-36 p-4">
-          <h1 className="text-bold dark:text-white">404</h1>
-          <h2 className="text-bold dark:text-white">
-            Oooops! The page cannot be found
-          </h2>
-          <p className="text-bold dark:text-white">
-            Redirecting to{' '}
+        <div className="dark:bg-personal_blue dark:shadow-gray-400 text-center relative top-1/3 h-36 p-4">
+          <h1 className="text-xl text-bold dark:text-white">404 | Not Found</h1>
+          <p className="text-md dark:text-white pt-6 tracking-wide">
+            Sorry. The requested URL /badpage was not found in this server.
+            <br />
+            Redirecting you to{' '}
             <a href="/">
               <span className="underline decoration-sky-500">Home page</span>...
+              in {time} seconds.
             </a>{' '}
           </p>
         </div>
       </section>
-      <FooterSection />
-    </>
+    </Layout>
   );
 }
