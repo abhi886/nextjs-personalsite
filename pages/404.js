da64910366add401913dde66a923d1ff88898830
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-04-23 12:31:28 +1000

    Remove unused declarations

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Head from 'next/dist/shared/lib/head';
 import { useRouter } from 'next/router';
 import Layout from '../src/components/LandingPageLayout/index';
 
