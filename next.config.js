90615fda4f8fca6e69680e73af5e53be322dcc5f
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-06-15 11:49:36 +1000

    Update to use svgr icon package

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
 };
