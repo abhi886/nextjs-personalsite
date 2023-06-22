a447506165daf9d5028dfe6794fffd7ab19cae7e
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-06-22 14:30:10 +1000

    Create work index page

---
+++ pages/works/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const work = () => {
+  return (
+    <section className="px-4 lg:px-48 min-h-screen lg:py-8 dark:bg-personal_blue">
+      <h1 className="text-center">
+        Work section is currently under construction. Apologies for the
+        inconvinience
+      </h1>
+    </section>
+  );
+};
+export default work;
