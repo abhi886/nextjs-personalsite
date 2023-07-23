0c5aec58cf49b661b18c71cd83e44dce3e96bc77
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-07-23 12:00:40 +1000

    Refactor: Unit test for Click to scroll component

--- __tests__/clickToScroll.test.jsx
+++ __tests__/clickToScroll.test.jsx
@@ -5,11 +5,11 @@ import ClickToScroll from '../src/components/ClickToScroll/index';
 test('1. Should display up button when link parameter is sent ', () => {
   render(<ClickToScroll linkUpward="#about" />);
   const element = screen.queryByTestId('upwardArrow');
-  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
 });
 
 test('2. Should not display any button when the parameter is not sent', () => {
   render(<ClickToScroll />);
   const element = screen.queryByTestId('upwardArrow');
-  expect(element).toBeFalsy();
+  expect(element).not.toBeInTheDocument();
 });
