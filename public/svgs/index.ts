2d1e03a60f28c460df617e1cf3f85f136d4402d0
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-08-22 09:38:33 +1000

    Add few Svg's and make it available for the app

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,3 +1,5 @@
 export { default as FacebookIcon } from './facebook.svg';
 export { default as LinkedInIcon } from './linkedin.svg';
 export { default as GitHubIcon } from './github.svg';
+export { default as EyeIcon } from './eye.svg';
+export { default as BookIcon } from './book.svg';
