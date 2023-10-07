f4c72a3e6e7d8bf2d9b96697511dce2e078ac692
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-10-07 11:32:27 +1100

    Fade in the div when user wants to read

--- src/components/TimelineDescription/index.jsx
+++ src/components/TimelineDescription/index.jsx
@@ -10,7 +10,13 @@ const index = ({ shortDescription, data }) => {
   };
   return (
     <div className="text-slate-500 ml-2" onClick={() => handleModalClick()}>
-      {!openModal ? <p>{shortDescription}</p> : <RichTextOptions data={data} />}
+      {!openModal ? (
+        <p>{shortDescription}</p>
+      ) : (
+        <div className="animate-fade-in-down">
+          <RichTextOptions data={data} />
+        </div>
+      )}
       <div className="text-slate-500 text-right">
         <button type="button">{!openModal ? 'Read More' : 'Read Less'}</button>
       </div>
