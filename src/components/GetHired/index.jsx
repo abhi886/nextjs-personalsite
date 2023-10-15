47e7d03e6b0b47982eae1ccaf652ec3ce2a6b2e9
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-10-16 09:45:50 +1100

    Fix typo

--- src/components/GetHired/index.jsx
+++ src/components/GetHired/index.jsx
@@ -25,7 +25,7 @@ export default function GetHired() {
               href="/contact"
               className="inline-flex items-center justify-center mt-6   py-2  overflow-hidden text-white transition-colors duration-300 bg-blue-600 rounded-lg shadow hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80"
             >
-              <span className="mx-2 px-1 py-1">LET'S TALK</span>
+              <span className="mx-2 px-1 py-1">LET&apos;S TALK</span>
             </Link>
           </div>
         </div>
