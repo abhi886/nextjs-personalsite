7e6791ff833ab8eaeaece364c2d4c94d28c55e13
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-05-17 11:19:04 +1000

    create work page

--- pages/works/[work].jsx
+++ pages/works/[work].jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import parse from 'html-react-parser';
+import { ClockIcon } from '@heroicons/react/solid';
 import query from '../../src/utils/queries/work-page-slug-query';
 import useContentful from '../../src/customHooks/use-contentful';
 import querySingleWorkPage from '../../src/utils/queries/work-page-query-props';
+import RichTextOptions from '../../src/utils/rich-text-options';
+import ClickToScroll from '../../src/components/ClickToScroll';
 
 export async function getStaticPaths() {
   const data = await useContentful(query);
@@ -31,18 +32,24 @@ export async function getStaticProps({ params: { work } }) {
 
 function aslug({ work: { workData } }) {
   const {
+    jobPosition,
     title,
-    workParser,
-    // blogImage: { url, width, height },
+    shortDescription,
+    siteUrl,
+    workDescription,
+    outcome,
+    coreApproach,
+    background,
+    language,
+    readTime,
   } = workData;
   return (
     <>
       <header className="relative min-h-[60vh] md:min-h-[70vh] flex items-center justify-center text-center text-white py-20 px-6">
         <div className="absolute inset-0 dark:bg-personal_blue bg-opacity-50 -z-10" />
-
         <div className="relative z-10 max-w-4xl mx-auto">
           <p className="text-sm uppercase tracking-wider font-semibold text-cyan-400 mb-2 md:mb-4">
-            Personal Project / Web Development
+            {jobPosition}
           </p>
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-6">
             <span className="text-gradient bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500 dark:text-white">
@@ -50,12 +57,11 @@ function aslug({ work: { workData } }) {
             </span>
           </h1>
           <p className="text-lg md:text-xl text-black dark:text-gray-200 max-w-3xl mx-auto">
-            Crafting a modern, performant, and easily updatable digital home to
-            reflect an evolving career in Full Stack Development & UI/UX Design.
+            {shortDescription}
           </p>
           <p className="mt-8">
             <a
-              href="https://abhishekhmaharjan.com"
+              href={siteUrl}
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-3 px-8 rounded-lg shadow-md transition-colors duration-300"
@@ -66,7 +72,66 @@ function aslug({ work: { workData } }) {
         </div>
       </header>
 
-      {parse(workParser)}
+      <main className="py-12 md:py-20">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 space-y-12 md:space-y-16">
+          <div className="flex-col md:flex justify-between items-center">
+            <div className="flex items-center dark:text-personal_blue-textParagraph">
+              <ClockIcon className="h-6" />
+              <p className="ml-1 text-sm">Read Time: {readTime} mins</p>
+            </div>
+
+            <div className="dark:text-personal_blue-textParagraph text-right">
+              {language && (
+                <p className="p-2 text-xs text-personal_blue-workBackground font-mono">
+                  Tech Stack:{' '}
+                  {language.map((lang) => (
+                    <span key={lang}>{lang} | </span>
+                  ))}
+                </p>
+              )}
+            </div>
+            <div className="flex justify-center text-personal_blue-textParagraph ">
+              <ClickToScroll
+                linkDownward="#backgound"
+                tooltipDownMessage="Background"
+              />
+            </div>
+          </div>
+
+          <article id="backgound">
+            <h2 className="text-3xl md:text-4xl font-bold mb-6 md:mb-8 pb-3 border-b-2 border-personal_blue-text inline-block">
+              Background
+            </h2>
+            <div className="max-w-none space-y-6">
+              <p>{background}</p>
+            </div>
+          </article>
+
+          <article>
+            <h2 className="text-3xl md:text-4xl font-bold mb-6 md:mb-8 pb-3 border-b-2 border-personal_blue-text inline-block">
+              The Solution
+            </h2>
+            <div className="space-y-6">
+              <RichTextOptions data={workDescription} />
+            </div>
+          </article>
+          <aside className="my-12 md:my-16 p-6 md:p-8 border-l-4 border-cyan-500 bg-gray-800 rounded-r-md shadow-lg">
+            <h3 className="text-xl font-semibold text-gray-100 mb-3">
+              My Core Approach
+            </h3>
+
+            <p className="mt-4 text-gray-400">{coreApproach}</p>
+          </aside>
+          <article>
+            <h2 className="text-3xl md:text-4xl font-bold mb-6 md:mb-8 pb-3 border-b-2 border-personal_blue-text inline-block">
+              The Outcome
+            </h2>
+            <div className="space-y-6">
+              <p className="">{outcome}</p>
+            </div>
+          </article>
+        </div>
+      </main>
     </>
   );
 }
--- src/utils/queries/work-page-query-props.js
+++ src/utils/queries/work-page-query-props.js
@@ -4,8 +4,55 @@ const querySingleWorkPage = `query GetWorksPostBySlug($slug: String!) {
    limit: 1) {
     items {
       title
+      jobPosition
+      shortDescription
+      siteUrl
+      workDescription {
+  json
+  links {
+    entries {
+      inline {
+        sys {
+          id
+        }
+        __typename
+        ... on CodeBlocks {
+          title
+          codeBlockContent
+        }
+      }
+      block {
+        sys {
+          id
+        }
+        __typename
+        ... on CodeBlocks {
+          title
+          codeBlockContent
+        }
+      }
+    }
+    assets {
+      block {
+        sys {
+          id
+        }
+        url
+        title
+        width
+        height
+        description
+      }
+    }
+  }
+}
+
       readTime
-      workParser
+      outcome
+      coreApproach
+      background
+      language
+      readTime
       blogImage {
         url
         width
